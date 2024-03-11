51ec89190e30302863629fea105e65bd1726595e
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-03-11 19:36:31 +0100

    Add SwiftTraderWebSocketError localization

---
+++ Sources/SwiftTraderWebSocket/Model/Error/SwiftTraderWebSocketError/SwiftTraderWebSocketError+Localization.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftTraderWebSocketError+Localization.swift
+//
+//
+//  Created by Fernando Fernandes on 28.02.24.
+//
+
+import Foundation
+
+extension SwiftTraderWebSocketError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .decodingMessage:
+            "There was an error while decoding a URLSessionWebSocketTask.Message."
+        case .encodingMessage:
+            "There was an error while encoding a URLSessionWebSocketTask.Message."
+        case .receivingMessage:
+            "There was an error while receiving a URLSessionWebSocketTask.Message."
+        }
+    }
+}
