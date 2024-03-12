c197e6122b6d2c56175f8a098a4ca4e025b29676
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-03-12 19:58:15 +0100

    Lint

--- Sources/ObservableWebSocketClient/Model/CodableWebSocketMessage/CodableWebSocketMessage.swift
+++ Sources/ObservableWebSocketClient/Model/CodableWebSocketMessage/CodableWebSocketMessage.swift
@@ -10,8 +10,4 @@ import Foundation
 /// Defines a `Codable` wrapper for `URLSessionWebSocketTask.Message`.
 public struct CodableWebSocketMessage: Codable {
     public let message: URLSessionWebSocketTask.Message
-
-    init(message: URLSessionWebSocketTask.Message) {
-        self.message = message
-    }
 }
