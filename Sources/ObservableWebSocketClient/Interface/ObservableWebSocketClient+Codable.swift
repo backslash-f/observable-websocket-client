cd747702c5084b085200280e3da4788884df4101
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-03-12 11:35:07 +0100

    Update codable confrormance

--- Sources/ObservableWebSocketClient/Interface/ObservableWebSocketClient+Codable.swift
+++ Sources/ObservableWebSocketClient/Interface/ObservableWebSocketClient+Codable.swift
@@ -9,8 +9,9 @@ import Foundation
 
 /// Required for encoding/decoding an observable object class. Refer to:
 /// https://www.hackingwithswift.com/books/ios-swiftui/encoding-an-observableobject-class
-extension ObservableWebSocketClient: Codable {
+extension ObservableWebSocketClient {
     public enum CodingKeys: String, CodingKey {
+        case id
         case websocketURL
         case message
         case error
@@ -18,6 +19,7 @@ extension ObservableWebSocketClient: Codable {
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
         try container.encode(websocketURL, forKey: .websocketURL)
         try container.encodeIfPresent(message, forKey: .message)
         try container.encodeIfPresent(error, forKey: .error)
@@ -25,9 +27,10 @@ extension ObservableWebSocketClient: Codable {
 
     public convenience init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        let id = try container.decode(UUID.self, forKey: .id)
         let websocketURL = try container.decode(URL.self, forKey: .websocketURL)
         let message = try container.decodeIfPresent(CodableWebSocketMessage.self, forKey: .message)
         let error = try container.decodeIfPresent(ObservableWebSocketClientError.self, forKey: .error)
-        self.init(websocketURL: websocketURL, message: message, error: error)
+        self.init(id: id, websocketURL: websocketURL, message: message, error: error)
     }
 }
