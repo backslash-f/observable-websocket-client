049ccac5ff8cc17f2c69194fee105806d2d81a5b
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-03-12 11:34:54 +0100

    Add UUID + convenience init

--- Sources/ObservableWebSocketClient/Interface/ObservableWebSocketClient.swift
+++ Sources/ObservableWebSocketClient/Interface/ObservableWebSocketClient.swift
@@ -9,12 +9,7 @@ import Foundation
 
 /// Establishes a WebSocket connection using the given `websocketURL`.
 /// Publishes received messages and errors.
-@Observable public final class ObservableWebSocketClient: Identifiable {
-
-    /// The WebSocket URL to connect to, starting with `wss`.
-    ///
-    /// E.g., `wss://endpoint.com`
-    public let websocketURL: URL
+@Observable public final class ObservableWebSocketClient: Identifiable, Codable, Equatable {
 
     /// Publishes received messages after the connection is successfully established.
     public var message: CodableWebSocketMessage?
@@ -22,11 +17,31 @@ import Foundation
     /// Publishes any error that may occur.
     public var error: ObservableWebSocketClientError?
 
-    public init(websocketURL: URL,
+    public var id: UUID
+
+    public let websocketURL: URL
+
+    private let service: ObservableWebSocketService
+
+    /// Creates an `ObservableWebSocketClient` instance.
+    ///
+    /// - Parameters:
+    ///   - websocketURL: The WebSocket `URL` to connect to, starting with `wss`.
+    ///   E.g., `wss://endpoint.com`
+    public convenience init(websocketURL: URL) {
+        self.init(websocketURL: websocketURL,
+                  message: nil,
+                  error: nil)
+    }
+
+    public init(id: UUID = .init(),
+                websocketURL: URL,
                 message: CodableWebSocketMessage? = nil,
                 error: ObservableWebSocketClientError? = nil) {
+        self.id = id
         self.websocketURL = websocketURL
         self.message = message
         self.error = error
+        self.service = ObservableWebSocketService(url: websocketURL)
     }
 }
