90f5d1cf84c084d3c6cd118c7bb3457f7acd156b
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-04-09 17:16:07 +0200

    Fix init with Error; add memberwise init

--- Sources/ObservableWebSocketClient/Model/Error/CodableError.swift
+++ Sources/ObservableWebSocketClient/Model/Error/CodableError.swift
@@ -10,10 +10,28 @@ import Foundation
 /// Defines a `Codable` wrapper for `Error`.
 public struct CodableError: Error, Codable, Equatable {
     public let errorType: String
-    public let message: String
+    public let description: String
+    public let localizedDescription: String
+    public let domain: String
+    public let code: Int
 
-    public init(_ error: Error, message: String? = nil) {
+    public init(_ error: Error) {
         self.errorType = String(reflecting: type(of: error))
-        self.message = message ?? error.localizedDescription
+        self.description = (error as NSError).description
+        self.localizedDescription = (error as NSError).localizedDescription
+        self.domain = (error as NSError).domain
+        self.code = (error as NSError).code
+    }
+    
+    internal init(errorType: String,
+                  description: String,
+                  localizedDescription: String,
+                  domain: String,
+                  code: Int) {
+        self.errorType = errorType
+        self.description = description
+        self.localizedDescription = localizedDescription
+        self.domain = domain
+        self.code = code
     }
 }
