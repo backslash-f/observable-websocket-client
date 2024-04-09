0b8e999d31ac22705b1b35557ae8c91338712417
Fernando Fernandes <mfernandes.fernando@gmail.com>
2024-04-09 17:09:05 +0200

    Add messageAsString extension

--- Sources/ObservableWebSocketClient/Model/CodableWebSocketMessage/CodableWebSocketMessage.swift
+++ Sources/ObservableWebSocketClient/Model/CodableWebSocketMessage/CodableWebSocketMessage.swift
@@ -8,6 +8,26 @@
 import Foundation
 
 /// Defines a `Codable` wrapper for `URLSessionWebSocketTask.Message`.
-public struct CodableWebSocketMessage: Equatable, Codable {
+public struct CodableWebSocketMessage: Equatable, Codable, Identifiable {
+    public let id = UUID()
     public let message: URLSessionWebSocketTask.Message
+    
+    public init(message: URLSessionWebSocketTask.Message) {
+        self.message = message
+    }
+}
+
+// MARK: - Interface
+
+public extension CodableWebSocketMessage {
+    func messageAsString() -> String {
+        switch message {
+        case .string(let text):
+            return text
+        case .data(let data):
+            return String(decoding: data, as: UTF8.self)
+        @unknown default:
+            return "Unknown message format"
+        }
+    }
 }
